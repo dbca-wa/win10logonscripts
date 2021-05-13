d7927d1bb7b7abc4f5c28fddac327df9b8490689
Adon Metcalfe <adonm@fastmail.fm>
2021-05-13 21:29:58 +0800

    Update default.ps1

--- default.ps1
+++ default.ps1
@@ -1,26 +1,22 @@
-# Refresh script from repo
-Invoke-WebRequest -Uri https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip -OutFile $scriptdir-repo.zip
-Remove-Item $scriptdir\* -Recurse -Force
-$ExtractShell = New-Object -ComObject Shell.Application 
-$Files = $ExtractShell.Namespace("$scriptdir-repo.zip").Items() 
-$ExtractShell.NameSpace($scriptdir).CopyHere($Files)
-Move-Item $scriptdir\win10logonscripts-main\* $scriptdir\ -Force
-mkdir $scriptdir/$env:username
-Get-ExecutionPolicy > $scriptdir/$env:username/test-execpolicy.txt
-$iselevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-write-output "iselevated: $iselevated"  >> $scriptdir/$env:username/test-execpolicy.txt
-
 # Run this script each logon elevated
-$scriptdir = "C:\ProgramData\dbca-gitlogon"
+$scriptdir = "$env:ProgramData\dbca-gitlogon"
+$updatezip = https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip
+
+# Refresh scripts from repo
+Invoke-WebRequest -Uri $updatezip -OutFile "$scriptdir.zip"
+Expand-Archive -Path $scriptdir.zip -DestinationPath $scriptdir -Force
+Remove-Item $scriptdir\scripts -Recurse -Force; Move-Item $scriptdir\win10logonscripts-main $scriptdir\scripts -Force
+Get-ExecutionPolicy > $scriptdir/$env:username-execpolicy.txt
+$iselevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+write-output "iselevated: $iselevated"  >> $scriptdir/$env:username-execpolicy.txt
 
 # Symlink onedrive folder
-cmd.exe /c rmdir c:\ProgramData\onedrive
-New-Item -ItemType Junction -Path C:\ProgramData\onedrive -Target $env:OneDriveCommercial
+$staticonedrive = "$env:ProgramData\onedrive"
+if (!(Test-Path $staticonedrive)) { New-Item -ItemType Junction -Path $staticonedrive -Target $env:OneDriveCommercial };
 
 # Update drive letter maps if set
-$mapdrivescmd = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\driveletters.cmd";
+$mapdrivescmd = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\driveletters.cmd";
 Remove-Item $mapdrivescmd -Force;
 foreach ($letter in $($($env:OneDriveLetterEmulation | Select-String -Pattern '^[A-Z]+$') -join "").toCharArray()) {
-    write-output "subst ${letter}: c:\programdata\onedrive" >> $mapdrivescmd
+    write-output "subst ${letter}: $staticonedrive" >> $mapdrivescmd
 }
-
