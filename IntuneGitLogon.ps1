517293b12c582f2b97a8def87213b6f4b0b174ba
Adon Metcalfe <adonm@fastmail.fm>
2021-05-13 21:32:08 +0800

    Update IntuneGitLogon.ps1

--- IntuneGitLogon.ps1
+++ IntuneGitLogon.ps1
@@ -1,12 +1,10 @@
 # Script to pull repo from github, and run repo/default.ps1 on user login as an elevated scheduled task
-$scriptdir = "C:\ProgramData\dbca-gitlogon"
+$scriptdir = "$env:ProgramData\dbca-gitlogon"
+$updatezip = https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip
 New-Item -ItemType Directory -Force -Path $scriptdir
-Invoke-WebRequest -Uri https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip -OutFile $scriptdir-repo.zip
-Remove-Item $scriptdir\* -Recurse -Force
-$ExtractShell = New-Object -ComObject Shell.Application 
-$Files = $ExtractShell.Namespace("$scriptdir-repo.zip").Items() 
-$ExtractShell.NameSpace($scriptdir).CopyHere($Files)
-Move-Item $scriptdir\win10logonscripts-main\* $scriptdir\ -Force
+Invoke-WebRequest -Uri $updatezip -OutFile "$scriptdir.zip"
+Expand-Archive -Path $scriptdir.zip -DestinationPath $scriptdir -Force
+Remove-Item $scriptdir\scripts -Recurse -Force; Move-Item $scriptdir\win10logonscripts-main $scriptdir\scripts -Force
 $Trigger = New-ScheduledTaskTrigger -AtLogon -User $env:username
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $scriptdir\default.ps1"
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $scriptdir\scripts\default.ps1"
 Register-ScheduledTask -TaskName $env:username"_logon" -Trigger $Trigger -User $env:username -Action $Action -RunLevel Highest -Force
