5b322f2dc86ec06d05d6de0bd1af9b8e5993aacf
Adon Metcalfe <adonm@fastmail.fm>
2021-05-13 16:14:47 +0800

    Update default.ps1

--- default.ps1
+++ default.ps1
@@ -1,4 +1,12 @@
-# Run this script all the time for all users
-Get-ExecutionPolicy > $env:userprofile/test-execpolicy.txt
+# Run this script each logon elevated
+$scriptdir = "C:\ProgramData\dbca-gitlogon"
+Invoke-WebRequest -Uri https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip -OutFile $scriptdir-repo.zip
+Remove-Item $scriptdir\* -Recurse -Force
+$ExtractShell = New-Object -ComObject Shell.Application 
+$Files = $ExtractShell.Namespace("$scriptdir-repo.zip").Items() 
+$ExtractShell.NameSpace($scriptdir).CopyHere($Files)
+Move-Item $scriptdir\win10logonscripts-main\* $scriptdir\ -Force
+mkdir $scriptdir/$env:username
+Get-ExecutionPolicy > $scriptdir/$env:username/test-execpolicy.txt
 $iselevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-write-output "iselevated: $iselevated"  >> $env:userprofile/test-execpolicy.txt
+write-output "iselevated: $iselevated"  >> $scriptdir/$env:username/test-execpolicy.txt
