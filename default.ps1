29e79a1af1c669f4e9aabc69fb118ded40a244a7
Adon Metcalfe <adonm@fastmail.fm>
2021-05-13 14:33:56 +0800

    Update default.ps1

--- default.ps1
+++ default.ps1
@@ -1,2 +1,4 @@
 # Run this script all the time for all users
 Get-ExecutionPolicy > $env:userprofile/test-execpolicy.txt
+$iselevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+write-output "iselevated: $iselevated"  >> $env:userprofile/test-execpolicy.txt
