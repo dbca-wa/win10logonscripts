08b2ea3dbec10914e837fe8b1aa9758b4553dbff
Adon Metcalfe <adonm@fastmail.fm>
2021-05-13 17:32:56 +0800

    Update default.ps1
    use user context for startup script to setup drive letters

--- default.ps1
+++ default.ps1
@@ -1,5 +1,4 @@
-# Run this script each logon elevated
-$scriptdir = "C:\ProgramData\dbca-gitlogon"
+# Refresh script from repo
 Invoke-WebRequest -Uri https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip -OutFile $scriptdir-repo.zip
 Remove-Item $scriptdir\* -Recurse -Force
 $ExtractShell = New-Object -ComObject Shell.Application 
@@ -11,19 +10,17 @@ Get-ExecutionPolicy > $scriptdir/$env:username/test-execpolicy.txt
 $iselevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 write-output "iselevated: $iselevated"  >> $scriptdir/$env:username/test-execpolicy.txt
 
+# Run this script each logon elevated
+$scriptdir = "C:\ProgramData\dbca-gitlogon"
+
 # Symlink onedrive folder
 cmd.exe /c rmdir c:\ProgramData\onedrive
 New-Item -ItemType Junction -Path C:\ProgramData\onedrive -Target $env:OneDriveCommercial
 
-# Map user env drive letters to it
+# Update drive letter maps if set
+$mapdrivescmd = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\driveletters.cmd";
+Remove-Item $mapdrivescmd -Force;
 foreach ($letter in $($($env:OneDriveLetterEmulation | Select-String -Pattern '^[A-Z]+$') -join "").toCharArray()) {
-    # Clear any network maps
-    Get-SmbMapping | Where-Object LocalPath -eq "${letter}:" | Remove-SmbMapping -Confirm:$false
-    # If drive is already mapped unmap it
-    if (Get-PSDrive | Where-Object Root -eq "${letter}:\") {
-        subst /D ${letter}: *>$null
-    # Otherwise map it
-    } else {
-        subst ${letter}: C:\ProgramData\onedrive
-    }
+    write-output "subst ${letter}: c:\programdata\onedrive" >> $mapdrivescmd
 }
+
