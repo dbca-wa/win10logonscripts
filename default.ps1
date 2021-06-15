d9fede9475760bef5240b8c399bd26d5d4c7b980
Adon Metcalfe <adonm@fastmail.fm>
2021-06-15 15:34:17 +0800

    Update default.ps1

--- default.ps1
+++ default.ps1
@@ -7,9 +7,9 @@ Invoke-WebRequest -Uri $updatezip -OutFile "$scriptdir/update.zip"
 Set-Location $scriptdir
 Expand-Archive update.zip -Force
 if (Test-Path "update\win10logonscripts-main\default.ps1") {
-    # Only update if zip extraction succeeded
-    if (Test-Path scripts) { Remove-Item scripts -Recurse -Force };
-    Move-Item update\* scripts -Force; Remove-Item update -Force;
+    # Only copy if unzip worked
+    robocopy "$scriptdir\update\win10logonscripts-main" "$scriptdir\scripts" /MIR;
+    Remove-Item update -Force; # cleanup update dir
 }
 Get-ExecutionPolicy > $scriptdir/$env:username-execpolicy.txt
 $iselevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
@@ -21,4 +21,4 @@ if (Test-Path $staticonedrive) { cmd.exe /c rmdir $staticonedrive };
 New-Item -ItemType Junction -Path $staticonedrive -Target $env:OneDriveCommercial;
 
 # Update shortcuts
-robocopy "$scriptdir/scripts/DBCA Utils" "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\DBCA Utils" /MIR
+robocopy "$scriptdir\scripts\DBCA Utils" "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\DBCA Utils" /MIR;
