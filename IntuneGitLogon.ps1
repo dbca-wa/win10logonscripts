e057018a6f45bd45e85f7687fdcc46297af37a80
Adon Metcalfe <adonm@fastmail.fm>
2021-06-23 10:26:05 +0800

    Update IntuneGitLogon.ps1
    Improve reliability

--- IntuneGitLogon.ps1
+++ IntuneGitLogon.ps1
@@ -1,12 +1,18 @@
 # Script to pull repo from github, and run repo/default.ps1 on user login as an elevated scheduled task
 $scriptdir = "$env:ProgramData\dbca-gitlogon"
 $updatezip = "https://github.com/dbca-wa/win10logonscripts/archive/refs/heads/main.zip"
-if (Test-Path $scriptdir) { Remove-Item $scriptdir -Recurse -Force }
-New-Item -ItemType Directory -Force -Path $scriptdir
+if (!$(Test-Path $scriptdir)) { New-Item -ItemType Directory -Force -Path $scriptdir }
+# Refresh scripts from repo
 Invoke-WebRequest -Uri $updatezip -OutFile "$scriptdir/update.zip"
 Set-Location $scriptdir
 Expand-Archive update.zip -Force
-if (Test-Path scripts) { Remove-Item scripts -Recurse -Force }; Move-Item update\* scripts -Force; Remove-Item update -Force;
+if (Test-Path "update\win10logonscripts-main\default.ps1") {
+    # Only copy if unzip worked
+    robocopy "$scriptdir\update\win10logonscripts-main" "$scriptdir\scripts" /MIR | Out-Null;
+    Remove-Item update -Force -Recurse; # cleanup update dir
+} else {
+    Throw 'Script download failed...';
+}
 $Trigger = New-ScheduledTaskTrigger -AtLogon -User $env:username
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $scriptdir\scripts\default.ps1"
 Register-ScheduledTask -TaskName $env:username"_logon" -Trigger $Trigger -User $env:username -Action $Action -RunLevel Highest -Force
