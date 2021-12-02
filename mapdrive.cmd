f9c7459d22641a2d3f9fac1f2cc55a03fa21a2bc
Kelly Thomas <kelly.thomas@hotmail.com.au>
2021-12-02 09:08:53 +0800

    use %OneDriveCommercial% for non-admin users

--- mapdrive.cmd
+++ mapdrive.cmd
@@ -1,2 +1,10 @@
 @echo off
-subst "%1:" "C:\ProgramData\onedrive"
+
+rem check for access to symlink
+dir "%ProgramData%\onedrive" >null 2>&1
+if %errorLevel% == 0 (
+    subst "%1:" "%ProgramData%\onedrive"
+) else (
+    rem no access to symlink so fall back to onedrive directory
+    subst "%1:" "%OneDriveCommercial%"
+)
